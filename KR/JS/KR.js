be13d884c32d679fb0503842bab6c0fd7b435c20
joshbunyan <78365838+joshbunyan@users.noreply.github.com>
2021-07-15 19:51:17 -0500

    sw text

--- KR/JS/KR.js
+++ KR/JS/KR.js
@@ -63,3 +63,25 @@ function openMenu(evt, menuName) {
     evt.currentTarget.firstElementChild.className += " kr-red";
   }
   document.getElementById("krLink").click();
+
+
+  /* Slide-Show text */
+  var myIndex = 0;
+    carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("sw-text");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+        myIndex = 1
+    }
+    
+    
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 4500);
+    
+}
